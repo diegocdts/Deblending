80976136962dcdf5f5f1d00129d65d3b2c89b1f1
Diego Souza <souza.claudiodiego@gmail.com>
2024-10-22 20:59:23 -0300

    pwd

--- run_model.py
+++ run_model.py
@@ -2,12 +2,12 @@ from model.training import TrainingValidation
 from util.arguments import args
 from util.config import load
 
-pwd = '/home/src/'
+pwd = '/home/src'
 args = args()
 data_name = args.data
 model_name = args.model
-data_parameters = load(f'config/data.json', data_name)
-model_parameters = load(f'config/model.json', model_name)
+data_parameters = load(f'{pwd}/config/data.json', data_name)
+model_parameters = load(f'{pwd}/config/model.json', model_name)
 
 training_validation = TrainingValidation(model_name, model_parameters, data_parameters)
 training_validation.train()
