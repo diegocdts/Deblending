484ceae4e004d01916499a8a423f878dd0d3e257
Diego Souza <souza.claudiodiego@gmail.com>
2024-10-24 13:30:51 -0300

    cross validation

--- model/training.py
+++ model/training.py
@@ -18,8 +18,7 @@ class TrainingValidation:
     def __init__(self, name, model_parameters, data_parameters):
         input_path = data_parameters["input_path"]
         target_path = data_parameters["target_path"]
-        n_train_files = data_parameters["n_train_files"]
-        n_val_files = data_parameters["n_val_files"]
+        n_train_val_files = data_parameters["n_train_val_files"]
         n_test_files = data_parameters["n_test_files"]
         outputs_path = data_parameters["outputs_path"]
 
@@ -27,23 +26,19 @@ class TrainingValidation:
         dropout_prob = model_parameters["dropout_prob"]
         n_splits = model_parameters["n_splits"]
 
-        start, end = 0, n_train_files
-        train_input, train_target = load_data(input_path, target_path, start, end)
+        start, end = 0, n_train_val_files
+        train_val_input, train_target = load_data(input_path, target_path, start, end)
 
-        start, end = n_train_files, (n_train_files + n_val_files)
-        val_input, val_target = load_data(input_path, target_path, start, end)
-
-        start, end = (n_train_files + n_val_files), (n_train_files + n_val_files + n_test_files)
+        start, end = n_train_val_files, (n_train_val_files + n_test_files)
         test_input, test_target = load_data(input_path, target_path, start, end)
 
         self.name = name
-        self.mean, self.std = mean_std(train_input)
+        self.mean, self.std = mean_std(train_val_input)
 
-        self.train_dataset = ImageDataset(nzs(train_input, self.mean, self.std), nzs(train_target, self.mean, self.std))
-        self.val_dataset = ImageDataset(nzs(val_input, self.mean, self.std), nzs(val_target, self.mean, self.std))
+        self.train_val_dataset = ImageDataset(nzs(train_val_input, self.mean, self.std), nzs(train_target, self.mean, self.std))
         self.test_dataset = ImageDataset(nzs(test_input, self.mean, self.std), nzs(test_target, self.mean, self.std))
 
-        self.model = MLP(units=train_input.shape[1], num_hidden_layers=num_hidden_layers, dropout_prob=dropout_prob)
+        self.model = MLP(units=train_val_input.shape[1], num_hidden_layers=num_hidden_layers, dropout_prob=dropout_prob)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
 
@@ -63,9 +58,9 @@ class TrainingValidation:
         Performs cross-validation over the model
         """
         val_losses = []
-        for fold, (train_index, val_index) in enumerate(self.k_fold.split(self.train_dataset)):
-            train_subset = Subset(self.train_dataset, train_index)
-            val_subset = Subset(self.val_dataset, val_index)
+        for fold, (train_index, val_index) in enumerate(self.k_fold.split(self.train_val_dataset)):
+            train_subset = Subset(self.train_val_dataset, train_index)
+            val_subset = Subset(self.train_val_dataset, val_index)
 
             train_loader = DataLoader(train_subset, batch_size=self.batch_size, shuffle=True)
             val_loader = DataLoader(val_subset, batch_size=self.batch_size, shuffle=False)
